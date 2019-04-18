8b68225847fc020f2744d31fa5d5ba54c934c545
王彦金 <wyj0056@175game.com>
2019-04-18 09:46:51 +0800

    fzf key bindings, hg prompt

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-set -x PATH $PATH /usr/sbin /sbin /usr/local/sbin /usr/local/bin ~/bin
+set -x PATH $PATH /usr/sbin /sbin /usr/local/sbin /usr/local/bin ~/bin ~/.local/bin
 #set -x LD_LIBRARY_PATH /home/wyj/w/lib
 
 #set -x GOROOT /usr/local/opt/go/libexec
@@ -38,11 +38,11 @@ set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
 
 # # Status Chars
-set __fish_git_prompt_char_dirtystate '±'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_stashstate '←'
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
+# set __fish_git_prompt_char_dirtystate '±'
+# set __fish_git_prompt_char_stagedstate '→'
+# set __fish_git_prompt_char_stashstate '←'
+# set __fish_git_prompt_char_upstream_ahead '↑'
+# set __fish_git_prompt_char_upstream_behind '↓'
 
 function fish_prompt
     set last_status $status
@@ -51,7 +51,8 @@ function fish_prompt
     set_color $fish_color_cwd
     printf '%s' (prompt_pwd)
     set_color normal
-    printf '%s ' (__fish_git_prompt)
+    printf '%s' (__fish_git_prompt)
+    printf '%s ' (__fish_hg_prompt)
     set_color normal
 end
 
